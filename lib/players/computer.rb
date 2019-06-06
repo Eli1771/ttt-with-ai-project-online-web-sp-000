80b5b042e947d0bb691b37a2fd6b7947e4382f62
elijahmm17 <elijahmm17@gmail.com>
2019-06-06 15:56:57 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,22 @@
+require 'pry'
+
 module Players 
   class Computer < Player 
+    
+    #looks for 2 of 3 win combo positions for a particular token
+    #3rd space in combo must be !taken?
+    #returns false if no almost win present 
+    #returns FIRST almost win detected
+    def almost_win(token)
+      combos = Game::WIN_COMBINATIONS
+      
+    end 
+    
     def move(board) 
-      rand(1..9).to_s
+      moves = ["1", "3", "5", "9", "7", "2", "4", "6", "8"]
+      moves.find {|m| !board.taken?(m)}
     end 
   end 
 end 
+
+#binding.pry
