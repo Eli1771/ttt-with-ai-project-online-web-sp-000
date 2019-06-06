a19332d1944a0d31df1e4925a3754e1d4821104a
elijahmm17 <elijahmm17@gmail.com>
2019-06-06 16:14:36 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,35 +2,6 @@ require 'pry'
 
 module Players 
   class Computer < Player 
-    
-    #looks for 2 of 3 win combo positions for a particular token
-    #3rd space in combo must be !taken?
-    #returns nil if no almost win present 
-    #returns FIRST almost win detected
-    def almost_win(token)
-      combos = Game::WIN_COMBINATIONS
-      board = Game.board.cells
-      combos.each do |c|
-        tokens = 0
-        blanks = 0 
-        win_space = ""
-        c.each do |position|
-          if board[position] == token
-            tokens += 1 
-          elsif board[position] == " "
-            blanks += 1 
-            win_space = (position + 1).to_s
-          end 
-        end 
-        #after each combo check for an almost win and reset counters
-        if tokens == 2 && blanks == 1 
-          return win_space
-          break
-        end 
-      end 
-      
-    end 
-    
     def move(board) 
       moves = ["1", "5", "3", "9", "7", "2", "4", "6", "8"]
       moves.find {|m| !board.taken?(m)}
