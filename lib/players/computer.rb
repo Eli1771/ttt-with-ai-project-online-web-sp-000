25fadde57f1c5910cf95af28c15392efa9a81ff4
elijahmm17 <elijahmm17@gmail.com>
2019-06-06 16:00:58 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -9,11 +9,17 @@ module Players
     #returns FIRST almost win detected
     def almost_win(token)
       combos = Game::WIN_COMBINATIONS
+      xs = 0
+      os = 0 
+      blanks = 0 
+      combos.each do |c|
+        
+      end 
       
     end 
     
     def move(board) 
-      moves = ["1", "3", "5", "9", "7", "2", "4", "6", "8"]
+      moves = ["1", "5", "3", "9", "7", "2", "4", "6", "8"]
       moves.find {|m| !board.taken?(m)}
     end 
   end 
